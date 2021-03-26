f13c2e653e824395a77df66bc3f6bab0a084d9bd
minibox24 <minibox724@gmail.com>
2021-03-26 20:53:19 +0900

    feat: main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,76 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        box
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="main">
+    <h1>{{ items.length }}</h1>
+    <div class="box-view">
+      <Box
+        v-for="(name, index) in items"
+        :key="index" :text="name"
+      />
+      <div class="add-box" @click="addItem">+</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      items: [],
+      fruits: [
+        "Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blueberry",
+        "Cherry", "Coconut", "Cucumber", "Durian", "Dragonfruit", "Fig",
+        "Gooseberry", "Grape", "Guava", "Jackfruit", "Plum", "Kiwifruit",
+        "Kumquat", "Lemon", "Lime", "Mango", "Watermelon", "Mulberry",
+        "Orange", "Papaya", "Passionfruit", "Peach", "Pear", "Persimmon",
+        "Pineapple", "Pineberry", "Quince", "Raspberry", "Soursop",
+        "Strawberry", "Tamarind", "Yuzu"
+      ]
+    }
+  },
+  methods: {
+    makeId() {
+      let id = ''
+      for (let i = 0; i < 2; i++) {
+        id += this.fruits[Math.floor(Math.random() * this.fruits.length)]
+      }
+      return id
+    },
+    addItem() {
+      this.items.push(this.makeId())
+    }
+  }
+}
 </script>
 
 <style>
-.container {
+.main {
   margin: 0 auto;
+  padding-top: 1rem;
   min-height: 100vh;
+  background: #eeeeee;
+  text-align: center;
+  user-select: none;
+}
+
+.box-view {
+  padding: 3rem;
+  display: grid;
+  grid-gap: 1.5rem;
+  place-items: center;
+  grid-template-rows: repeat(auto-fill, minmax(10rem, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+}
+
+.add-box {
+  width: 10rem;
+  height: 10rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  border-radius: 10%;
+  border: 5px dashed black;
+  font-weight: bold;
+  font-size: 7rem;
+  cursor: pointer;
 }
 </style>
